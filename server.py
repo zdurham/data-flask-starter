178a698ccc9d6c65c9d88f28744006dbc1c97bea
Zach Durham <zdurham101@gmail.com>
2020-01-04 14:27:22 -0500

    adding environment variable

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, jsonify;
+import os
+port = os.environ.get('PORT')
 
 app =  Flask(__name__)
 
@@ -12,4 +14,10 @@ def getData():
   return jsonify([1, 2, 3, 4, 5, 6, 7])
 
 if __name__ == "__main__":
-    app.run()
+  if port:
+    app.run(port=os.environ.get('PORT'))
+  else:
+    app.run(port=5000, debug=True)
+
+  
+    
